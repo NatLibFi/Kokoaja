2d9b68827c47bdd8c22ff819e99a6b8bde275e91
Joeli Takala <joeli.takala@helsinki.fi>
2024-01-26 16:35:19 +0200

    KOKO päivitetty

--- testKoko.sh
+++ testKoko.sh
@@ -1,9 +1,10 @@
 service fuseki start
+cd ~/git/Finto-data
+git pull
 cd ~/git/Kokoaja
 cp ~/eclipse-workspace/Kokoaja/src/*.java .
 ./compile.sh
 ./run_local.sh
 cd ~/git/Finto-data/vocabularies/koko
 ./toskos_test.sh
-curl -X POST http://localhost:3030/test?update=DROP%20ALL
 /opt/fuseki/bin/s-put http://localhost:3030/test http://www.yso.fi/onto/koko-testi/ ~/git/Finto-data/vocabularies/koko/koko-skos_test.ttl
