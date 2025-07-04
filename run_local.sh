664515e1d50815e7efaaed31fe3d0eee63cb850e
Joeli Takala <joeli.takala@helsinki.fi>
2025-07-04 16:27:05 +0300

    KOKO päivitetty

--- run_local.sh
+++ run_local.sh
@@ -2,9 +2,9 @@
 
 VASTAAVUUDET=kokoUriVastaavuudet.txt
 VASTAAVUUDETUUDET=kokoUriVastaavuudet-test.txt
-YSO=../Finto-data/vocabularies/yso/yso-skos.ttl
+YSO=../Finto-data/vocabularies/yso/yso-vb-skos.ttl
 KOOSTUMUS=conf/kokoKoostumus.txt
-KOKOOLD=../Finto-data/vocabularies/koko/koko-skos.ttl
+KOKOOLD=../Finto-data/vocabularies/koko/koko-skos_edellinen.ttl
 KOKONEW=../Finto-data/vocabularies/koko/koko-test.ttl
 MUSTALISTA=conf/kokoMustalista.txt
 REPLACEDBY=../Finto-data/vocabularies/koko/koko-replacedby.ttl
