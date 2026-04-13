02325799300b86c650ca1d897fe0f550537a7f3b
Joeli Pokkinen <joeli.pokkinen@helsinki.fi>
2026-04-13 18:35:05 +0300

    Päivitetty testi-KOKOn komentosarja uudelle Fusekille

--- testKoko.sh
+++ testKoko.sh
@@ -1,10 +1,16 @@
-~/fuseki/fuseki-server &
+#! /bin/bash
+
+~/fuseki/fuseki restart
 cd ~/git/Finto-data
 git pull
 cd ~/git/Kokoaja
-cp ~/eclipse-workspace/Kokoaja/src/main/java/*.java .
 ./compile.sh
 ./run_local.sh
 cd ~/git/Finto-data/vocabularies/koko
 ./toskos_test.sh
-~/fuseki/bin/s-put http://localhost:3030/test http://www.yso.fi/onto/koko-testi/ ~/git/Finto-data/vocabularies/koko/koko-skos_test.ttl
+
+DATASET='http://localhost:3030/test'
+graph='http://www.yso.fi/onto/koko-testi/'
+type='text/turtle'
+
+curl -sT ~/git/Finto-data/vocabularies/koko/koko-skos_test.ttl -H "Content-Type: $type" "$DATASET?graph=$graph"
